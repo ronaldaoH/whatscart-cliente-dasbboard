e045a4b3ee0cd7bb8eed7382fcc995b21d18efc6
Ronaldao <ronaldao@rankea.pe>
2020-06-12 22:32:43 -0500

    first commit

---
+++ precache-manifest.20a4248d12fe8f4fc7ea7618a4526253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa3a68bd2a911838716844b38f45d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2b3718a2e9495ec6631",
+    "url": "/static/css/2.aff4ec99.chunk.css"
+  },
+  {
+    "revision": "5c52bb7dccee11c5c691",
+    "url": "/static/css/main.97049754.chunk.css"
+  },
+  {
+    "revision": "a2b3718a2e9495ec6631",
+    "url": "/static/js/2.ad106921.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad106921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c52bb7dccee11c5c691",
+    "url": "/static/js/main.31b5067e.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
